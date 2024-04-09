d8b82d6690b8c4995aee84af0e720d6c610e1691
wind831016 <fish88mm@gmail.com>
2024-04-09 16:11:22 +0800

    練習登入畫面

---
+++ Login/js/main_Login_001.js
@@ -0,0 +1,11 @@
+$(function () {
+  $("#loginBtn").click(function () {
+    alert("hello world");
+  });
+
+  /* 動畫   
+  $(".message a").click(function () {
+    $("form").animate({ height: "toggle", opacity: "toggle" }, "slow");
+  });
+  */
+});
